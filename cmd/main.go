da3f00294b3c9cd31af8a09b47daceb488c165cd
Víctor Pérez <vmpch1@outlook.com>
2025-04-12 21:49:14 -0600

    DAPA-33 | feat: create register endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"dapa/app/handlers"
+	"dapa/app/utils"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 func main() {
@@ -19,19 +22,25 @@ func main() {
 		AllowCredentials: true,
 	}))
 
+	// Implementación de los custom validators
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("validrole", utils.RoleValidator)
+		v.RegisterValidation("password", utils.PasswordValidator)
+		v.RegisterValidation("phone", utils.PhoneValidator)
+	}
+
 	// Ping endpoint para probar conexión
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "¡Backend conectado con éxito!",
 		})
 	})
-	
 
 	// Endpoints de API reales
 	api := router.Group("/api")
 	{
-		api.POST("/user", handlers.CreateUser)
-		api.GET("/user", handlers.GetUsers)
+		api.POST("/users", handlers.RegisterHandler)
+		api.GET("/users", handlers.GetUsers)
 	}
 
 	// Iniciar servidor
